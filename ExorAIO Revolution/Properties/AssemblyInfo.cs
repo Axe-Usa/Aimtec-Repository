4d0d852542a39da5e0d247b192d6bb90b7cbc012
Exory <enrico.drago3@gmail.com>
2017-08-21 21:53:30 +0200

    Updated changelogs.

--- ExorAIO Revolution/Properties/AssemblyInfo.cs
+++ ExorAIO Revolution/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("7.16.3.9")]
-[assembly: AssemblyFileVersion("7.16.3.9")]
+[assembly: AssemblyVersion("7.16.3.10")]
+[assembly: AssemblyFileVersion("7.16.3.10")]
