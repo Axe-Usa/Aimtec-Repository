ae3a607c07af9b4adb8c6e3fc3cfe688083ff90b
Exory <enrico.drago3@gmail.com>
2017-09-11 02:16:54 +0200

    ExorAIO: Fixed CompilingError.

--- ExorAIO Revolution/AIO/Utilities/Helpers/Geometry.cs
+++ ExorAIO Revolution/AIO/Utilities/Helpers/Geometry.cs
@@ -36,9 +36,9 @@ namespace AIO.Utilities
 
             var solution = new Paths();
             var c = new Clipper();
-            c.AddPaths(subj, PolyType.ptSubject, true);
-            c.AddPaths(clip, PolyType.ptClip, true);
-            c.Execute(ClipType.ctUnion, solution, PolyFillType.pftPositive, PolyFillType.pftEvenOdd);
+            c.AddPaths(subj, PolyType.PtSubject, true);
+            c.AddPaths(clip, PolyType.PtClip, true);
+            c.Execute(ClipType.CtUnion, solution, PolyFillType.PftPositive, PolyFillType.PftEvenOdd);
             return solution;
         }
 
