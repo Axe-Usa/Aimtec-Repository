ce4874bac0ef222f2d56e9877a9306e84612d485
Exory <enrico.drago3@gmail.com>
2017-09-27 14:08:54 +0200

    ExorAIO: 7.19.0.0
    Orianna: Updated for 7.19 Updates.

--- ExorAIO Revolution/AIO/Champions/Orianna/Utilities/Spells.cs
+++ ExorAIO Revolution/AIO/Champions/Orianna/Utilities/Spells.cs
@@ -18,11 +18,11 @@ namespace AIO.Champions
         public void Spells()
         {
             SpellClass.Q = new Spell(SpellSlot.Q, 825f);
-            SpellClass.W = new Spell(SpellSlot.W, float.MaxValue);
+            SpellClass.W = new Spell(SpellSlot.W);
             SpellClass.E = new Spell(SpellSlot.E, 1100f);
-            SpellClass.R = new Spell(SpellSlot.R, float.MaxValue);
+            SpellClass.R = new Spell(SpellSlot.R);
 
-            SpellClass.Q.SetSkillshot(0f, 80f, 1200f, false, SkillshotType.Circle);
+            SpellClass.Q.SetSkillshot(0f, 80f, 1400f, false, SkillshotType.Circle);
             SpellClass.W.SetSkillshot(0.25f, 255f, 3200f, false, SkillshotType.Circle);
             SpellClass.E.SetSkillshot(0f, 85f, 1850f, false, SkillshotType.Line);
             SpellClass.R.SetSkillshot(0.75f, 410f, 1000f, false, SkillshotType.Circle);
--- ExorAIO Revolution/Properties/AssemblyInfo.cs
+++ ExorAIO Revolution/Properties/AssemblyInfo.cs
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("7.18.0.7")]
-[assembly: AssemblyFileVersion("7.18.0.7")]
+[assembly: AssemblyVersion("7.19.0.0")]
+[assembly: AssemblyFileVersion("7.19.0.0")]
