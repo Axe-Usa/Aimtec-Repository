b507dc86d705fe19530f33b0b9c3f283fc966d81
Exory <enrico.drago3@gmail.com>
2017-11-18 00:45:37 +0100

    Updated changelogs.

--- ExorAIO Revolution/AIO/Champions/Lucian/Modes/Weaving.cs
+++ ExorAIO Revolution/AIO/Champions/Lucian/Modes/Weaving.cs
@@ -17,6 +17,68 @@ namespace AIO.Champions
         #region Public Methods and Operators
 
         /// <summary>
+        ///     The E Combo Logic.
+        /// </summary>
+        public void ELogic(Obj_AI_Hero target)
+        {
+            if (UtilityClass.Player.Distance(Game.CursorPos) <= UtilityClass.Player.AttackRange &&
+                MenuClass.Spells["e"]["customization"]["onlyeifmouseoutaarange"].As<MenuBool>().Enabled)
+            {
+                return;
+            }
+
+            var posAfterE = UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, 300f);
+            var eRangeCheck = MenuClass.Spells["e"]["customization"]["erangecheck"];
+            if (eRangeCheck != null)
+            {
+                if (eRangeCheck.As<MenuSliderBool>().Enabled &&
+                    posAfterE.CountEnemyHeroesInRange(UtilityClass.Player.AttackRange + UtilityClass.Player.BoundingRadius) >= eRangeCheck.As<MenuSliderBool>().Value)
+                {
+                    return;
+                }
+            }
+
+            if (posAfterE.Distance(target) >
+                    UtilityClass.Player.GetFullAttackRange(target) &&
+                MenuClass.Spells["e"]["customization"]["noeoutaarange"].As<MenuBool>().Enabled)
+            {
+                return;
+            }
+
+            if (posAfterE.PointUnderEnemyTurret() &&
+                MenuClass.Spells["e"]["customization"]["noeturret"].As<MenuBool>().Enabled)
+            {
+                return;
+            }
+
+            switch (MenuClass.Spells["e"]["mode"].As<MenuList>().Value)
+            {
+                case 0:
+                    Vector3 point;
+                    if (UtilityClass.Player.IsUnderEnemyTurret() ||
+                        UtilityClass.Player.Distance(Game.CursorPos) < UtilityClass.Player.AttackRange)
+                    {
+                        point = UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, UtilityClass.Player.BoundingRadius);
+                    }
+                    else
+                    {
+                        point = UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, 475f);
+                    }
+
+                    SpellClass.E.Cast(point);
+                    return;
+
+                case 1:
+                    SpellClass.E.Cast(UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, 425f));
+                    return;
+
+                case 2:
+                    SpellClass.E.Cast(UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, UtilityClass.Player.BoundingRadius));
+                    return;
+            }
+        }
+
+        /// <summary>
         ///     Called on do-cast.
         /// </summary>
         /// <param name="sender">The sender.</param>
@@ -39,61 +101,8 @@ namespace AIO.Champions
                     if (SpellClass.E.Ready &&
                         MenuClass.Spells["e"]["combo"].As<MenuBool>().Enabled)
                     {
-                        if (UtilityClass.Player.Distance(Game.CursorPos) <= UtilityClass.Player.AttackRange &&
-                            MenuClass.Spells["e"]["customization"]["onlyeifmouseoutaarange"].As<MenuBool>().Enabled)
-                        {
-                            return;
-                        }
-
-                        var posAfterE = UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, 300f);
-                        var eRangeCheck = MenuClass.Spells["e"]["customization"]["erangecheck"];
-                        if (eRangeCheck != null)
-                        {
-                            if (eRangeCheck.As<MenuSliderBool>().Enabled &&
-                                posAfterE.CountEnemyHeroesInRange(UtilityClass.Player.AttackRange + UtilityClass.Player.BoundingRadius) >= eRangeCheck.As<MenuSliderBool>().Value)
-                            {
-                                return;
-                            }
-                        }
-
-                        if (posAfterE.Distance(heroTarget) >
-                                UtilityClass.Player.GetFullAttackRange(heroTarget) &&
-                            MenuClass.Spells["e"]["customization"]["noeoutaarange"].As<MenuBool>().Enabled)
-                        {
-                            return;
-                        }
-
-                        if (posAfterE.PointUnderEnemyTurret() &&
-                            MenuClass.Spells["e"]["customization"]["noeturret"].As<MenuBool>().Enabled)
-                        {
-                            return;
-                        }
-
-                        switch (MenuClass.Spells["e"]["mode"].As<MenuList>().Value)
-                        {
-                            case 0:
-                                Vector3 point;
-                                if (UtilityClass.Player.IsUnderEnemyTurret() ||
-                                    UtilityClass.Player.Distance(Game.CursorPos) < UtilityClass.Player.AttackRange)
-                                {
-                                    point = UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, UtilityClass.Player.BoundingRadius);
-                                }
-                                else
-                                {
-                                    point = UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, 475f);
-                                }
-
-                                SpellClass.E.Cast(point);
-                                return;
-
-                            case 1:
-                                SpellClass.E.Cast(UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, 425f));
-                                return;
-
-                            case 2:
-                                SpellClass.E.Cast(UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, UtilityClass.Player.BoundingRadius));
-                                return;
-                        }
+                        ELogic(heroTarget);
+                        return;
                     }
                     break;
 
@@ -145,61 +154,8 @@ namespace AIO.Champions
                     if (SpellClass.E.Ready &&
                         MenuClass.Spells["e"]["combo"].As<MenuBool>().Enabled)
                     {
-                        if (UtilityClass.Player.Distance(Game.CursorPos) <= UtilityClass.Player.AttackRange &&
-                            MenuClass.Spells["e"]["customization"]["onlyeifmouseoutaarange"].As<MenuBool>().Enabled)
-                        {
-                            return;
-                        }
-
-                        var posAfterE = UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, 300f);
-                        var eRangeCheck = MenuClass.Spells["e"]["customization"]["erangecheck"];
-                        if (eRangeCheck != null)
-                        {
-                            if (eRangeCheck.As<MenuSliderBool>().Enabled &&
-                                posAfterE.CountEnemyHeroesInRange(UtilityClass.Player.AttackRange + UtilityClass.Player.BoundingRadius) >= eRangeCheck.As<MenuSliderBool>().Value)
-                            {
-                                return;
-                            }
-                        }
-
-                        if (posAfterE.Distance(heroTarget) >
-                                UtilityClass.Player.GetFullAttackRange(heroTarget) &&
-                            MenuClass.Spells["e"]["customization"]["noeoutaarange"].As<MenuBool>().Enabled)
-                        {
-                            return;
-                        }
-
-                        if (posAfterE.PointUnderEnemyTurret() &&
-                            MenuClass.Spells["e"]["customization"]["noeturret"].As<MenuBool>().Enabled)
-                        {
-                            return;
-                        }
-
-                        switch (MenuClass.Spells["e"]["mode"].As<MenuList>().Value)
-                        {
-                            case 0:
-                                Vector3 point;
-                                if (UtilityClass.Player.IsUnderEnemyTurret() ||
-                                    UtilityClass.Player.Distance(Game.CursorPos) < UtilityClass.Player.AttackRange)
-                                {
-                                    point = UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, UtilityClass.Player.BoundingRadius);
-                                }
-                                else
-                                {
-                                    point = UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, 475f);
-                                }
-
-                                SpellClass.E.Cast(point);
-                                return;
-
-                            case 1:
-                                SpellClass.E.Cast(UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, 425f));
-                                return;
-
-                            case 2:
-                                SpellClass.E.Cast(UtilityClass.Player.ServerPosition.Extend(Game.CursorPos, UtilityClass.Player.BoundingRadius));
-                                return;
-                        }
+                        ELogic(heroTarget);
+                        return;
                     }
                     break;
 
